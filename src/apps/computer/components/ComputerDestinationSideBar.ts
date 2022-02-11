b91652f8c052c71b630842acb2c42d8e6447f835
Martin Rossil <martinrossil@gmail.com>
2022-02-11 15:45:05 +0100

    Created Destinations Sidebar.

---
+++ src/apps/computer/components/ComputerDestinationSideBar.ts
@@ -0,0 +1,25 @@
+import { DataContainer, VerticalLayout } from 'enta';
+import Model from '../../../state/Model';
+import Colors from '../../../theme/Colors';
+import Shadows from '../../../theme/Shadows';
+import DestinationLink from '../../../vo/DestinationLink';
+import ComputerDestinationLinkRenderer from './ComputerDestinationLinkRenderer';
+
+export default class ComputerDetinationsSideBar extends DataContainer<DestinationLink> {
+    public constructor() {
+        super();
+        this.name = 'ComputerDetinationsSideBar';
+        this.width = 272;
+        this.percentHeight = 100;
+        this.top = 72;
+        this.paddingTop = 16;
+        this.paddingX = 8;
+        this.backgroundColor = Colors.WHITE;
+        this.addFilter(Shadows.BOX_SHADOW_RIGHT_1);
+        this.addFilter(Shadows.BOX_SHADOW_RIGHT_2);
+        this.layout = new VerticalLayout(8);
+        this.DataRendererClass = ComputerDestinationLinkRenderer;
+        this.dataProvider = Model.destinations;
+    }
+}
+customElements.define('computer-destinations-side-bar', ComputerDetinationsSideBar);
