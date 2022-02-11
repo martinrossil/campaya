f005874ef5cbb280579914f43501cca1f64c64d5
Martin Rossil <martinrossil@gmail.com>
2022-02-11 17:02:38 +0100

    Added interfaces.

--- src/theme/Colors.ts
+++ src/theme/Colors.ts
@@ -3,10 +3,10 @@ import { Color, IColor } from 'enta';
 export default class Colors {
     public static WHITE: IColor = new Color(0, 0, 100);
     public static BLACK: IColor = new Color(0, 0, 0);
-    public static NEUTRAL_50 = new Color(0, 0, 98);
-    public static NEUTRAL_100 = new Color(0, 0, 96);
-    public static NEUTRAL_600 = new Color(0, 0, 32);
-    public static BRAND = new Color(161, 100, 33);
-    public static BRAND_LIGHT = new Color(161, 70, 55);
-    public static BRAND_DARK = new Color(161, 100, 24);
+    public static NEUTRAL_50: IColor = new Color(0, 0, 98);
+    public static NEUTRAL_100: IColor = new Color(0, 0, 96);
+    public static NEUTRAL_600: IColor = new Color(0, 0, 32);
+    public static BRAND: IColor = new Color(161, 100, 33);
+    public static BRAND_LIGHT: IColor = new Color(161, 70, 55);
+    public static BRAND_DARK: IColor = new Color(161, 100, 24);
 }
