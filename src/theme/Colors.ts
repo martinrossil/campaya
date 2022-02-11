b9fd6ba48ad42b92d1982e36b05a5c7874e53256
Martin Rossil <martinrossil@gmail.com>
2022-02-11 10:30:02 +0100

    Created Colors.

---
+++ src/theme/Colors.ts
@@ -0,0 +1,6 @@
+import { Color, IColor } from 'enta';
+
+export default class Colors {
+    public static WHITE: IColor = new Color(0, 0, 100);
+    public static NEUTRAL_50 = new Color(0, 0, 98);
+}
