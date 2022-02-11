defbfac136060796b4837aa03f5534bc6b7c6886
Martin Rossil <martinrossil@gmail.com>
2022-02-11 15:43:01 +0100

    Added Poppin font.

--- rollup.production.js
+++ rollup.production.js
@@ -108,13 +108,42 @@ function template({ files }) {
     <meta name="Description" content="Sommerhusudlejning | Lej sommerhus fra alle bureauer her">
     <meta name="theme-color" content="#036">
     <meta name="robots" content="noindex">
+    <link rel="preload" href="Poppins-Regular.woff2" as="font" type="font/woff2" crossorigin>
+    <link rel="preload" href="Poppins-Bold.woff2" as="font" type="font/woff2" crossorigin>
+    <link rel="preload" href="Poppins-Medium.woff2" as="font" type="font/woff2" crossorigin>
     <link rel="icon" href="icon32x32.ico">
     <link rel="icon" href="icon.svg" type="image/svg+xml">
     <link rel="apple-touch-icon" href="icon180x180.png">
     <link rel="manifest" href="manifest.webmanifest">
     <title>Sommerhusudlejning | Lej sommerhus fra alle bureauer her</title>
+    <style>
+        @font-face {
+          font-family: 'Poppins';
+          src: url('Poppins-Regular.woff2') format('woff2'), url('Poppins-Regular.woff') format('woff');
+          font-weight: normal;
+          font-style: normal;
+          font-display: block;
+        }
+        @font-face {
+            font-family: 'Poppins';
+            src: url('Poppins-Bold.woff2') format('woff2'), url('Poppins-Bold.woff') format('woff');
+            font-weight: bold;
+            font-style: normal;
+            font-display: block;
+        }
+        @font-face {
+            font-family: 'Poppins';
+            src: url('Poppins-Medium.woff2') format('woff2'), url('Poppins-Medium.woff') format('woff');
+            font-weight: 500;
+            font-style: normal;
+            font-display: block;
+        }
+    </style>
   </head>
   <body>
+    <h1 style="font-family: 'Poppins', sans-serif; visibility: hidden; font-weight: normal;">Camapaya</h1>
+    <h2 style="font-family: 'Poppins', sans-serif; visibility: hidden; font-weight: bold;">Sommerhusudlejning</h2>
+    <h3 style="font-family: 'Poppins', sans-serif; visibility: hidden; font-weight: 500;">Lej sommerhus fra alle bureauer her</h3>
     <campaya-app></campaya-app>
     <script>
         function c(){if(window.fetch)d();else{var b=document.createElement("script");b.src="./fetch.js";b.onload=d;document.body.appendChild(b)}}function d(){var b=document.createElement("script");b.type="module";if("function"===typeof Object.fromEntries)var a="` + entryChunk2019 + `";else void 0!==window.Promise&&"function"===typeof window.Promise.prototype.finally?a="` + entryChunk2018 + `":"function"===typeof String.prototype.padStart?a="` + entryChunk2017 + `":"function"===typeof Array.prototype.includes?a="` + entryChunk2016 + `":void 0!==window.Promise?a="` + entryChunk2015 + `":(a="` + entryChunkES5 + `",b.type="");b.src=a;document.body.appendChild(b)}if("function"!==typeof window.CustomEvent){var CustomEvent=function(b,a){a=a||{bubbles:!1,cancelable:!1,detail:void 0};var f=document.createEvent("CustomEvent");f.initCustomEvent(b,a.bubbles,a.cancelable,a.detail);return f};CustomEvent.prototype=window.Event.prototype;window.CustomEvent=CustomEvent}if(window.customElements)c();else{var e=document.createElement("script");e.src="./ce.js";e.onload=c;document.body.appendChild(e)};
