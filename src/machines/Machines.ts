76f8f0fed04ad357fbb9da8f1d6547330bc1c41b
Martin Rossil <martinrossil@gmail.com>
2022-02-11 15:46:14 +0100

    Added NavigationMachine.

--- src/machines/Machines.ts
+++ src/machines/Machines.ts
@@ -1,10 +1,13 @@
 import CampayaApp from '../CampayaApp';
+import NavigationMachine from './NavigationMachine';
 import ResponsiveMachine from './ResponsiveMachine';
 
 export default class Machines {
     public constructor(host: CampayaApp) {
         this.responsiveMachine = new ResponsiveMachine(host);
+        this.navigationMachine = new NavigationMachine(host);
     }
 
     private responsiveMachine: ResponsiveMachine;
+    private navigationMachine: NavigationMachine;
 }
