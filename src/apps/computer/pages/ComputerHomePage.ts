b441b35b742d5197565f2095be02fe396c4fe42a
Martin Rossil <martinrossil@gmail.com>
2022-02-11 15:45:24 +0100

    Added children.

--- src/apps/computer/pages/ComputerHomePage.ts
+++ src/apps/computer/pages/ComputerHomePage.ts
@@ -1,11 +1,13 @@
 import { DisplayContainer } from 'enta';
+import BackgroundLogo from '../../shared/BackgroundLogo';
+import ComputerDetinationsSideBar from '../components/ComputerDestinationSideBar';
 
 export default class ComputerHomePage extends DisplayContainer {
     public constructor() {
         super();
         this.name = 'ComputerHomePage';
-        this.top = 72;
         this.percentWidth = this.percentHeight = 100;
+        this.addElements([new ComputerDetinationsSideBar(), new BackgroundLogo()]);
     }
 }
 customElements.define('computer-home-page', ComputerHomePage);
