8c794ef0230ccb2b8382dccd8b04c511b8d487f9
Martin Rossil <martinrossil@gmail.com>
2022-02-11 15:44:22 +0100

    Added typefaces.

---
+++ src/theme/Typography.ts
@@ -0,0 +1,7 @@
+import { ITypeFace, TypeFace } from 'enta';
+
+export default class Typography {
+    public static POPPINS_REGULAR: ITypeFace = new TypeFace('Poppins', 0.71, 0.08, 0.0);
+    public static POPPINS_MEDIUM: ITypeFace = new TypeFace('Poppins', 0.71, 0.08, 0.0);
+    public static POPPINS_BOLD: ITypeFace = new TypeFace('Poppins', 0.71, 0.06, 0.0);
+}
