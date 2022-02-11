43ee3cc32b973c02e3a3e43bc89fa4b2cd182cd2
Martin Rossil <martinrossil@gmail.com>
2022-02-11 17:01:49 +0100

    Updated.

--- src/apps/computer/pages/ComputerHomePage.ts
+++ src/apps/computer/pages/ComputerHomePage.ts
@@ -1,13 +1,15 @@
-import { DisplayContainer } from 'enta';
-import BackgroundLogo from '../../shared/BackgroundLogo';
+import { DisplayContainer, HorizontalLayout } from 'enta';
 import ComputerDetinationsSideBar from '../components/ComputerDestinationSideBar';
+import ComputerFilterSideBar from '../components/ComputerFilterSideBar';
+import ComputerProperties from '../components/ComputerProperties';
 
 export default class ComputerHomePage extends DisplayContainer {
     public constructor() {
         super();
         this.name = 'ComputerHomePage';
         this.percentWidth = this.percentHeight = 100;
-        this.addElements([new ComputerDetinationsSideBar(), new BackgroundLogo()]);
+        this.layout = new HorizontalLayout();
+        this.addElements([new ComputerDetinationsSideBar(), new ComputerProperties(), new ComputerFilterSideBar()]);
     }
 }
 customElements.define('computer-home-page', ComputerHomePage);
