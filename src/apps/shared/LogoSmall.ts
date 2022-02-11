563365815f72278bd05de5a8c336f996bd2a182e
Martin Rossil <martinrossil@gmail.com>
2022-02-11 15:46:29 +0100

    Renamed.

--- src/apps/shared/LogoSmall.ts
+++ src/apps/shared/LogoSmall.ts
@@ -3,14 +3,14 @@ import Colors from '../../theme/Colors';
 import { Icons } from '../../theme/Icons';
 import IconElement from './IconElement';
 
-export default class Logo extends IconElement {
+export default class LogoSmall extends IconElement {
     public constructor() {
         super();
-        this.name = 'Logo';
+        this.name = 'LogoSmall';
         this.size(56, 56);
         this.fillColor = Colors.BRAND;
         this.pathData = Icons.LOGO_SMALL;
         this.viewBox = new ViewBox(-14, -16, 141, 140);
     }
 }
-customElements.define('logo-small', Logo);
+customElements.define('logo-small', LogoSmall);
