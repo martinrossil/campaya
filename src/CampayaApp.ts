ff1eb9cf310808c994d9e648d9c13645f8481c84
Martin Rossil <martinrossil@gmail.com>
2022-02-11 10:30:30 +0100

    Added Machines.

--- src/CampayaApp.ts
+++ src/CampayaApp.ts
@@ -1,9 +1,14 @@
 import { ApplicationElement } from 'enta';
+import Machines from './machines/Machines';
+import Colors from './theme/Colors';
 
 export default class CampayaApp extends ApplicationElement {
     public constructor() {
         super();
         this.style.height = '100vh';
+        document.body.style.backgroundColor = Colors.NEUTRAL_50.toString();
     }
+
+    private machines = new Machines(this);
 }
 customElements.define('campaya-app', CampayaApp);
