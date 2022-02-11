5700a8f9ccf08bd9ccc58dde0ee6651c625fbca6
Martin Rossil <martinrossil@gmail.com>
2022-02-11 11:29:43 +0100

    Moved apps.

--- src/apps/computer/ComputerApp.ts
+++ src/apps/computer/ComputerApp.ts
@@ -1,10 +1,13 @@
 import { DisplayContainer } from 'enta';
+import ComputerAppBar from './components/ComputerAppBar';
+import ComputerHomePage from './pages/ComputerHomePage';
 
 export default class ComputerApp extends DisplayContainer {
     public constructor() {
         super();
         this.name = 'ComputerApp';
         this.percentWidth = this.percentHeight = 100;
+        this.addElements([new ComputerAppBar(), new ComputerHomePage()]);
     }
 }
 customElements.define('computer-app', ComputerApp);
