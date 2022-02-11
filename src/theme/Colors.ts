f9c6c6f76735c49167bedb364630266b731941d7
Martin Rossil <martinrossil@gmail.com>
2022-02-11 11:30:47 +0100

    Added BRAND color.

--- src/theme/Colors.ts
+++ src/theme/Colors.ts
@@ -3,4 +3,5 @@ import { Color, IColor } from 'enta';
 export default class Colors {
     public static WHITE: IColor = new Color(0, 0, 100);
     public static NEUTRAL_50 = new Color(0, 0, 98);
+    public static BRAND = new Color(161, 100, 33);
 }
