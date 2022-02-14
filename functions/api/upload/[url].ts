a564ebd4efc2e625a6f48a994f6c8ae005be2acf
Martin Rossil <martinrossil@gmail.com>
2022-02-14 12:42:43 +0100

    WIP

--- functions/api/upload/[url].ts
+++ functions/api/upload/[url].ts
@@ -1,7 +1,7 @@
 /* eslint-disable */
 export async function onRequest({request}): Promise<Response> {
     // return new Response(JSON.stringify(await request.body.text()), responseInit);
-    const body = request.text();
+    const body = await request.text();
     return new Response(body, {
         headers: {
             'content-type': 'text/plain',
