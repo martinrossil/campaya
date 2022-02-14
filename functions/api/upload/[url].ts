a088d4364d75338730746d05febd0e380c06f96f
Martin Rossil <martinrossil@gmail.com>
2022-02-14 12:21:33 +0100

    WIP

--- functions/api/upload/[url].ts
+++ functions/api/upload/[url].ts
@@ -1,12 +1,11 @@
 /* eslint-disable */
-const responseInit = {
-    headers: {
-        'Access-Control-Allow-Origin': '*',
-        'content-type': 'text/plain'
-        //'content-type': 'application/json;charset=UTF-8'
-    }
-}
-
 export async function onRequest({request}): Promise<Response> {
-    return new Response(JSON.stringify(await request.body.text()), responseInit);
+    // return new Response(JSON.stringify(await request.body.text()), responseInit);
+    const body = await request.body.text();
+    return new Response(body, {
+        headers: {
+            'content-type': 'text/plain',
+            'Access-Control-Allow-Origin': '*',
+        }
+    });
 }
