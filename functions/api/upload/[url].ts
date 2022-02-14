7a13789300a120ec77bd105db4c38bc83058f733
Martin Rossil <martinrossil@gmail.com>
2022-02-14 12:50:20 +0100

    WIP

--- functions/api/upload/[url].ts
+++ functions/api/upload/[url].ts
@@ -1,10 +1,16 @@
 /* eslint-disable */
-export async function onRequest({request}): Promise<Response> {
+export async function onRequest({request, env}): Promise<Response> {
     // return new Response(JSON.stringify(await request.body.text()), responseInit);
-    const body = await request.text();
+    const text = await request.text();
+    const body = JSON.stringify({
+        text,
+        fauna: env.FAUNA_TOKEN,
+        images: env.IMAGES_TOKEN
+    });
     return new Response(body, {
         headers: {
-            'content-type': 'text/plain',
+            'content-type': 'application/json;charset=UTF-8',
+            // 'content-type': 'text/plain',
             'Access-Control-Allow-Origin': '*',
         }
     });
