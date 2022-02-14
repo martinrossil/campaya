d50be8f471fcfeaff109ff65a5618906c1acb008
Martin Rossil <martinrossil@gmail.com>
2022-02-14 22:59:49 +0100

    Created properties endpoint.

---
+++ functions/api/properties.ts
@@ -0,0 +1,38 @@
+import { PropertiesRequest } from '../types/PropertiesRequest'
+
+/* eslint-disable */
+export async function onRequest({env, request}): Promise<Response> {
+    try {
+        const propertiesRequest: PropertiesRequest = await request.json();
+        const response: Response = await fetch('https://graphql.fauna.com/graphql', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                authorization: 'Basic ' + env.FAUNA_TOKEN
+            },
+            body: `{
+                "query": "query properties {
+                    properties(_size: ${propertiesRequest.size}, _cursor: ${propertiesRequest.cursor}) {
+                        data {
+                            bedrooms
+                            description
+                            img
+                            fromPrice
+                            id
+                            bathrooms
+                            type
+                            sleeps
+                            toPrice
+                            destinations
+                        }
+                        before
+                        after
+                    }
+                }"
+            }`
+        });
+        return new Response(await response.json());
+    } catch (error) {
+        return new Response(JSON.stringify(error));
+    }
+}
---
+++ functions/types/PropertiesRequest.ts
@@ -0,0 +1,4 @@
+export type PropertiesRequest = {
+    size: number,
+    cursor: string | null
+}
