ba5f4d119d3eee75cc4f2983d581dcb8d8c16f92
Martin Rossil <martinrossil@gmail.com>
2022-02-15 12:51:30 +0100

    Created updateProperty endpoint.

---
+++ functions/api/updateProperty.ts
@@ -0,0 +1,56 @@
+import { UpdatePropertyRequest } from '../types/UpdatePropertyRequest';
+
+/* eslint-disable */
+export async function onRequest({env, request}): Promise<Response> {
+    try {
+        const updatePropertyRequest: UpdatePropertyRequest = await request.json();
+        const id = updatePropertyRequest.id;
+        const property = updatePropertyRequest.propertyInput;
+        const query = JSON.stringify({
+            query: `mutation {
+                updateProperty(id: "${id}", data: {
+                    bathrooms: ${property.bathrooms}
+                    bedrooms: ${property.bedrooms}
+                    description: "${property.description}"
+                    destinations: ${JSON.stringify(property.destinations)}
+                    fromPrice: ${property.fromPrice}
+                    id: "${property.id}"
+                    img: "${property.img}"
+                    sleeps: ${property.sleeps}
+                    picture: "${property.picture}"
+                    toPrice: ${property.toPrice}
+                    type: "${property.type}"
+                })
+                {
+                    bathrooms
+                    bedrooms
+                    description
+                    destinations
+                    fromPrice
+                    id
+                    img
+                    sleeps
+                    picture
+                    toPrice
+                    type
+                }
+            }`
+        });
+        const response: Response = await fetch('https://graphql.fauna.com/graphql', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                authorization: 'Basic ' + env.FAUNA_TOKEN,
+            },
+            body: query
+        });
+        return new Response(JSON.stringify(await response.json()), {
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            }
+        });
+    } catch (error) {
+        return new Response(JSON.stringify(error));
+    }
+}
