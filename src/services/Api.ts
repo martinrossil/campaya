9fa14ccc2a97e1db5914e25cc541b8af63511349
Martin Rossil <martinrossil@gmail.com>
2022-03-03 13:13:46 +0100

    Added image upload.

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -1,7 +1,25 @@
 import { CreateAuthenticatedDirectUploadResponse } from '../types/CreateAuthenticatedDirectUploadResponse';
-import { Properties } from '../types/Properties';
+import { PropertiesResponse } from '../types/PropertiesResponse';
+import { Property } from '../types/Property';
+import { UploadImageResponse } from '../types/UploadImageResponse';
 
-export async function properties(size = 12, cursor: string | null = null): Promise<[Properties | null, Error | null]> {
+export async function updateProperty(property: Property): Promise<[Property | null, Error | null]> {
+    try {
+        const response: Response = await fetch('https://campaya.pages.dev/api/updateProperty', {
+            method: 'POST',
+            body: JSON.stringify(property)
+        });
+        if (response.ok) {
+            const result = await response.json();
+            return [result, null];
+        }
+        return [null, new Error('Response Error')];
+    } catch (error) {
+        return catchError(error);
+    }
+}
+
+export async function properties(size = 12, cursor: string | null = null): Promise<[PropertiesResponse | null, Error | null]> {
     try {
         const response: Response = await fetch('https://campaya.pages.dev/api/properties', {
             method: 'POST',
@@ -11,7 +29,7 @@ export async function properties(size = 12, cursor: string | null = null): Promi
             })
         });
         if (response.ok) {
-            const result: Properties = await response.json();
+            const result: PropertiesResponse = await response.json();
             return [result, null];
         }
         return [null, new Error('Response Error')];
@@ -28,6 +46,39 @@ export async function createAuthenticatedDirectUpload(): Promise<[CreateAuthenti
         if (response.ok) {
             const result: CreateAuthenticatedDirectUploadResponse = await response.json();
             return [result, null];
+        }
+        return [null, new Error('Response Error')];
+    } catch (error) {
+        return catchError(error);
+    }
+}
+
+export async function getPropertyImageBlob(img: string): Promise<[Blob | null, Error | null]> {
+    try {
+        const response: Response = await fetch(img);
+        if (response.ok) {
+            return [await response.blob(), null];
+        }
+        return [null, new Error('Response Error')];
+    } catch (error) {
+        return catchError(error);
+    }
+}
+
+export async function uploadImage(uploadURL: string, blob: Blob, filename: string): Promise<[UploadImageResponse | null, Error | null]> {
+    try {
+        const file = new File([blob], 'blob.jpg', { type: 'image/jpeg', lastModified: new Date().getTime() });
+        const data: FormData = new FormData();
+        data.append('file', file, filename);
+        const response = await fetch(uploadURL, {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json'
+            },
+            body: data
+        });
+        if (response.ok) {
+            return [await response.json(), null];
         }
         return [null, new Error('Response Error')];
     } catch (error) {
